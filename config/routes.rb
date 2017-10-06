e5f85496fcfe6e074e84de3fac12a57633f63029
tracykaur <tracykaur@yahoo.com>
2017-10-06 16:16:12 +1000

    made links

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   # get '/rooms'
+  get '/home', {to: 'pages#home', as: 'home'}
   get '/rooms', {to: 'pages#rooms', as: 'rooms'}
   get '/contact', {to: 'pages#contact', as: 'contact'}
+  get '/about', {to: 'pages#about', as: 'about'}
+
   # get '/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
