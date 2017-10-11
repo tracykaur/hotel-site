3c7a1939581fe6b845b9691eb68f9a9d5165d85c
tracykaur <tracykaur@yahoo.com>
2017-10-11 13:33:57 +1000

    Added contact form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,17 @@ class PagesController < ApplicationController
     @enquiry = Enquiry.new
   end
 
-  def about
+  def create
+    @enquiry = Enquiry.create(post_params)
+    redirect_to '/', notice: 'Message was successfully sent.'
   end
+
+
+  private
+
+  def post_params
+    params.require(:enquiry).permit(:name, :email, :phone, :message)
+  end
+
 
 end
